51f759a83f0cadb3f8e4b041240efe27ed9aa028
Marcello Barisonzi <eltorpe@hotmail.com>
2016-01-19 17:37:27 -0500

    using xbmcswift

--- default.py
+++ default.py
@@ -1,16 +1,22 @@
 import sys
 import os
+from xbmc import Keyboard
+from xbmcaddon import Addon
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
 import urllib
 import re
 
+from xbmcswift2 import Plugin
+
+plugin = Plugin()
+
 #addon name
-__addonname__ = 'plugin.video.myvideo'
+__addonname__ = plugin.addon.getAddonInfo('id')
 
 #get path the default.py is in.
-__addonpath__ = xbmcaddon.Addon(id=__addonname__).getAddonInfo('path')
+__addonpath__ = plugin.addon.getAddonInfo('path')
 
 #datapath
 __datapath__ = xbmc.translatePath('special://profile/addon_data/'+__addonname__)
@@ -25,21 +31,10 @@ import gethtml
 def get_html(url):
     return gethtml.get(url, __datapath__)
 
-addon_handle = int(sys.argv[1])
+#addon_handle = int(sys.argv[1])
 
-xbmcplugin.setContent(addon_handle, 'movies')
+#xbmcplugin.setContent(addon_handle, 'videos')
 
-#urls = ["http://www.xvideos.com/video11036330/0/splendid_mom_ariella_ferrera_gets_nailed_on_the_couch",
-#        "http://www.xvideos.com/video14481755/0/asian_hottie_cindy_starfall_fucks_and_gets_a_creampie",]
-        #"http://www.xvideos.com/video8991790/beata_and_vanessa_sweet_punishment",
-        #"http://www.xvideos.com/video10455445/vanessa_sky_was_all_set_and_ready_to_hit",
-        #"http://www.xvideos.com/video6912062/sweet_exgf_swallow",
-        #"http://www.xvideos.com/video7792032/big_juggs_teen_august_ames_gets_her_pussy_railed_by_big_cock",
-        #"http://www.xvideos.com/video9688257/august_ames_gets_fucked_in_the_bathroom",
-        #"http://www.xvideos.com/video9472632/busty_amateur_girlfriend_pleasing_dude",
-        #"http://www.xvideos.com/video4312598/passionate_love_making_for_this_lustfilled_couple",
-        #"http://www.xvideos.com/video10639164/mia_khalifa_interracial_threesome",
-        #]"""
 
 def getItems(url):
     print "Opening", url
@@ -53,17 +48,99 @@ def getItems(url):
     print 'image: %s' % img[0]
     return tit[0], fetchurl, img[0]
 
-r = get_html("http://xvideos.com")
-
-ptn = re.compile("<a href=\"(/video.+?)\"><img")
-urls = ["http://xvideos.com/%s" % i.strip() for i in ptn.findall(r)]
-
-
-for u in urls:
-    t,f,i = getItems(u)
-
-    li = xbmcgui.ListItem(t, iconImage=i, path=f)
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=f, listitem=li)
+@plugin.route('/')
+def main_menu():
+    items = [{'label': "Show Latest Videos", 'path': plugin.url_for('show_latest')},
+             {'label': "Show Best Videos", 'path': plugin.url_for('show_best')},
+             {'label': "Search Videos", 'path': plugin.url_for('search')},
+             ]
+    
+    return items
 
 
-xbmcplugin.endOfDirectory(addon_handle)
+@plugin.route('/show_latest')
+def show_latest():
+    r = get_html("http://xvideos.com")
+
+    ptn = re.compile("<a href=\"(/video.+?)\"><img")
+    urls = ["http://xvideos.com/%s" % i.strip() for i in ptn.findall(r)]
+
+    items = []
+    for u in urls:
+        t,f,i = getItems(u)
+
+        items.append({'label': t, 'path': plugin.url_for("play_video",url=f), 'thumbnail' : i})
+        
+    return items
+
+@plugin.route('/show_best')
+def show_best():
+    r = get_html("http://xvideos.com/best")
+
+    ptn = re.compile("<a href=\"(/video.+?)\"><img")
+    urls = ["http://xvideos.com/%s" % i.strip() for i in ptn.findall(r)]
+
+    items = []
+    for u in urls:
+        t,f,i = getItems(u)
+
+        items.append({'label': t, 'path': plugin.url_for("play_video",url=f), 'thumbnail' : i})
+        
+    return items
+
+@plugin.route('/profiles/<nm>')
+def show_profile(nm):
+    print "Inside show_profile(%s)" % nm
+    
+    print "Opening %s" % ("http://xvideos.com/profiles/%s/videos" % nm)
+    r = get_html("http://xvideos.com/profiles/%s/videos" % nm)
+
+    ptn = re.compile('<a href="(/prof-video-click.+?)"><img')
+    urls = ["http://xvideos.com/%s" % i.strip() for i in ptn.findall(r)]
+
+    items = []
+    for u in urls:
+        t,f,i = getItems(u)
+
+        items.append({'label': t, 'path': plugin.url_for("play_video",url=f), 'thumbnail' : i})
+        
+    return items
+
+@plugin.route('/search')
+def search():
+    kb = Keyboard('', "Insert search term")
+    kb.doModal()
+    if (kb.isConfirmed()):
+        search_item = urllib.quote(kb.getText().strip())
+    else:
+        return
+    
+    r = get_html("http://xvideos.com/?k=%s" % search_item)
+
+    # see if there's a profile
+    pro_ptn = re.compile('<img src="(http://.+?jpg)" data-videos=\'\[\]\' /></a></div><p><a href="(/profiles/.+?)">(.+?)</a>')
+
+    items = []
+
+    if pro_ptn.search(r):
+        icon,lnk,nm = pro_ptn.findall(r)[0]
+        lnk =  plugin.url_for('show_profile',nm=os.path.basename(lnk))
+        nm = "[Profile] %s" % nm       
+        items.append({'label': nm, 'path': lnk, 'thumbnail' : icon}) 
+
+    ptn = re.compile("<a href=\"(/video.+?)\"><img")
+    urls = ["http://xvideos.com/%s" % i.strip() for i in ptn.findall(r)]
+
+    for u in urls:
+        t,f,i = getItems(u)
+
+        items.append({'label': t, 'path': plugin.url_for("play_video",url=f), 'thumbnail' : i})
+        
+    return items
+
+@plugin.route('/play_video/<url>/')
+def play_video(url):
+    plugin.log.info('Playing url: %s' % url)
+    plugin.set_resolved_url(url)
+
+plugin.run()
